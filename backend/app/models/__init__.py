5ec942d204e4e927ae06028fa454cdf42f0d8737
arkk200 <arkk20000@gmail.com>
2026-03-28 10:17:43 +0900

    refactor: 리뷰 문서 워크플로 제거

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,11 +1,9 @@
 from app.models.extraction_job import ExtractionJob
-from app.models.review_record import ReviewRecord
 from app.models.saved_puzzle import SavedPuzzle
 from app.models.upload import Upload
 
 __all__ = [
     "Upload",
     "ExtractionJob",
-    "ReviewRecord",
     "SavedPuzzle",
 ]
